f96455f6fef43040f8a40ccfab329d963932ccb8
urireo <mary.pinedar@gmail.com>
2019-12-04 00:20:24 +0000

    hello_00 -Odin Project

--- 00_hello/hello.rb
+++ 00_hello/hello.rb
@@ -1,1 +1,11 @@
 #write your code here
+def hello()
+    "Hello!"
+end
+def greet(who)
+    "Hello, #{who}!"
+end
+
+hello()
+greet ("Alice")
+greet ("Bob")
--- 00_hello/hello_spec.rb
+++ 00_hello/hello_spec.rb
@@ -111,7 +111,6 @@ describe "the greet function" do
   it "says hello to someone" do
     expect(greet("Alice")).to eq("Hello, Alice!")
   end
-
   it "says hello to someone else" do
     expect(greet("Bob")).to eq("Hello, Bob!")
   end
